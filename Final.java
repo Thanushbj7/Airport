274a74fa13870e0194926b84df983fd93298497e
Thanushbj7 <111106003+Thanushbj7@users.noreply.github.com>
2024-02-19 19:28:13 +0530

    Update Final.java

--- Final.java
+++ Final.java
@@ -1,5 +1,40 @@
 <template>
     <div style="height: 300px;">
+        <h2>
+            <lightning-icon icon-name="action:new_note" title=" Case History"></lightning-icon>
+            <b>Case History</b>
+        </h2>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <template for:each={columns} for:item="col">
+                        <th key={col.label}>{col.label}</th>
+                    </template>
+                </tr>
+            </thead>
+            <tbody>
+                <template for:each={data} for:item="item" for:index="index">
+                    <tr key={item.Id}>
+                        <td>{index + rowOffset}</td>
+                        <template for:each={columns} for:item="col">
+                            <td key={col.field}>{item[col.field]}</td>
+                        </template>
+                    </tr>
+                </template>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+
+
+
+
+
+<template>
+    <div style="height: 300px;">
         <h2> 
             <lightning-icon icon-name="action:new_note" title=" Case History"></lightning-icon>
             <b>Case History</b>
