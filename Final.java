83846d576941897a43df2cf9cf073672805cbc4a
Thanushbj7 <111106003+Thanushbj7@users.noreply.github.com>
2024-02-20 11:36:18 +0530

    Update Final.java

--- Final.java
+++ Final.java
@@ -1,3 +1,178 @@
+import { LightningElement, api, track, wire } from 'lwc';
+import { publish, MessageContext } from 'lightning/messageService';
+import EXAMPLE_MESSAGE_CHANNEL from '@salesforce/messageChannel/ExampleMessageChannel__c';
+import fetchWrapperCases from '@salesforce/apex/caseHistoryLWC.fetchWrapperCases';
+
+const columns = [
+    { label: 'Case Number', fieldName: 'caseNumber' },
+    { label: 'Date', fieldName: 'createdDate' },
+    { label: 'Plan Id', fieldName: 'planId' },
+    { label: 'Inquiry', fieldName: 'callTypeInquiry', columnKey: 'inq' },
+    { label: 'Transactions', fieldName: 'callTypeTransaction', columnKey: 'tra' },
+    { label: 'Account Maintenance', fieldName: 'callTypeAccountMaintenance', columnKey: 'accM' },
+    { label: 'Forms', fieldName: 'callTypeForms', columnKey: 'for' },
+    { label: 'Others', fieldName: 'callTypeOthers', columnKey: 'oth' },
+];
+
+export default class newLWC extends LightningElement {
+    @track data = [];
+    @track columns = columns;
+
+    wiredRecords;
+
+
+    connectedCallback() {
+            // Parse the URL and get the 'passedValue' parameter
+            const urlParams = new URLSearchParams(window.location.search);
+            this.clientSSN = urlParams.get('clientSSN');
+            console.log('ssn should appear here' + this.clientSSN);
+            console.log('inside connected callback');
+        }
+        //{SSN:this.clientSSN}
+
+    @wire(fetchWrapperCases) wiredCases(value) {
+        //console.log("Columns stringified 1st  --" , this.columns);
+        this.wiredRecords = value;
+        const { data, error } = value;
+        if (data) {
+
+            let tempRecords = JSON.parse(JSON.stringify(data));
+            let uniqueCaseNumbers = new Set();
+
+            let uniqueCallTypesMap = new Map();
+
+            function groupBy(objectArray, property) {
+                return objectArray.reduce(function(acc, obj) {
+                    var key = obj[property];
+                    if (!acc[key]) {
+                        acc[key] = [];
+                    }
+                    acc[key].push(obj);
+                    return acc;
+                }, {});
+            }
+            var groupedPlanIds;
+            var groupedCaseNumbers = groupBy(tempRecords, 'caseNumber');
+            let final = [];
+            let val = Object.values(groupedCaseNumbers);
+            console.log("Object.values--", val);
+            console.log("groupedCaseNumbers!", groupedCaseNumbers);
+            let cak = [];
+            let copiedarr = [];
+            for (let i = 0; i < val.length; i++) {
+
+                //	console.log("Object.values for palnId--" , val[i]);
+                groupedPlanIds = groupBy(val[i], 'planId')
+                let planVal = Object.values(groupedPlanIds)
+                console.log("Object.values for grouped PlanIds--", planVal);
+
+                for (let j = 0; j < planVal.length; j++) {
+                    let uniquePlanIds = new Set();
+                    let sObjectArray = [];
+                    const filteredCaseNumber = [];
+                    const filteredCreatedDate = [];
+                    const filteredPlanId = [];
+                    const filteredCallTypeInquiry = [];
+                    const filteredCallTypeTransaction = [];
+                    const filteredCallTypeAccountMaintenance = [];
+                    const filteredCallTypeForms = [];
+                    const filteredCallTypeOthers = [];
+                    console.log("planVal[j]--", planVal[j]);
+                    planVal[j].forEach(sObject => {
+                        filteredCaseNumber.push(sObject.caseNumber);
+                        filteredCreatedDate.push(sObject.createdDate);
+                        filteredPlanId.push(sObject.planId);
+                        if (sObject.callTypeInquiry) {
+                            filteredCallTypeInquiry.push(sObject.callTypeInquiry);
+                        }
+                        if (sObject.callTypeTransaction) {
+                            filteredCallTypeTransaction.push(sObject.callTypeTransaction);
+                        }
+                        if (sObject.callTypeAccountMaintenance) {
+                            filteredCallTypeAccountMaintenance.push(sObject.callTypeAccountMaintenance);
+                        }
+                        if (sObject.callTypeForms) {
+                            filteredCallTypeForms.push(sObject.callTypeForms);
+                        }
+                        if (sObject.callTypeOthers) {
+                            filteredCallTypeOthers.push(sObject.callTypeOthers);
+                        }
+                    });
+                    /*	console.log("filteredCaseNumber--" ,filteredCaseNumber);
+                    	console.log("filteredCreatedDate--" ,filteredCreatedDate);
+                    	console.log("filteredPlanId--" ,filteredPlanId);
+                    	console.log("filteredCallTypeInquiry--" ,filteredCallTypeInquiry);
+                    	console.log("filteredCallTypeTransaction--" ,filteredCallTypeTransaction);
+                    	console.log("filteredCallTypeAccountMaintenance--" ,filteredCallTypeAccountMaintenance);
+                    	console.log("filteredCallTypeForms--" ,filteredCallTypeForms);
+                    	console.log("filteredCallTypeOthers--" ,filteredCallTypeOthers);*/
+                    var arrLength = filteredCaseNumber.length;
+                    console.log("arrLength--", arrLength);
+                    for (let i = 0; i < arrLength; i++) {
+                        let title = {};
+                        title.caseNumber = filteredCaseNumber[i];
+                        title.createdDate = filteredCreatedDate[i];
+                        title.planId = filteredPlanId[i];
+                        title.callTypeInquiry = filteredCallTypeInquiry[i];
+                        title.callTypeTransaction = filteredCallTypeTransaction[i];
+                        title.callTypeAccountMaintenance = filteredCallTypeAccountMaintenance[i];
+                        title.callTypeForms = filteredCallTypeForms[i];
+                        title.callTypeOthers = filteredCallTypeOthers[i];
+                        sObjectArray.push(title);
+                        //		console.log("title--" ,title);
+                    }
+                    console.log("sObjectArray--", sObjectArray);
+                    cak = sObjectArray.map(row => {
+                        if (!uniquePlanIds.has(row.planId)) {
+                            uniquePlanIds.add(row.planId);
+                            return {...row, caseNumber: row.caseNumber, createdDate: row.createdDate };
+                            //		copiedarr.push(cak);
+                        }
+                        return {...row, caseNumber: null, createdDate: null, planId: null };
+                        //copiedarr.push(cak);
+                        //console.log("copiedarr--" ,copiedarr);
+                    });
+
+
+                    copiedarr.push(cak);
+
+                }
+
+            }
+            console.log("copiedarr--", copiedarr);
+
+            const flattenedArr = copiedarr.flat(1);
+            console.log("flattenedArr--", flattenedArr);
+            let kk = flattenedArr.filter(sObj => {
+                if (sObj.callTypeInquiry != null || sObj.callTypeTransaction != null || sObj.callTypeAccountMaintenance != null || sObj.callTypeForms != null || sObj.callTypeOthers != null) {
+                    return sObj;
+                }
+            });
+            this.data = kk;
+
+            console.log("kk!", kk);
+        }
+
+        if (error) {
+            console.log("error Occurred!", error);
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 <template>
     <div style="height: 300px;">
         <h2>
