700674f31272a5c9a4dd53437d61e7824dc75801
Thanushbj7 <111106003+Thanushbj7@users.noreply.github.com>
2024-04-10 16:41:58 +0530

    Update CopyTableExtract.java

--- CopyTableExtract.java
+++ CopyTableExtract.java
@@ -1,3 +1,60 @@
+private BalanceHistoryCache(final ParticipantKey key) {
+    ParticipantInfo partInfo = ParticipantInfo.getInstance(key.getClient(), key.getPlan(), key.getParticipant());
+    int pptAcctAge = partInfo.getParticipantAccountAgeInMonths();
+    startDate = Calendar.getInstance();
+    endDate = Calendar.getInstance();
+
+    if (pptAcctAge >= 24) {
+        logDebug(key, "Participant's account age is >= 24 months, making a call to SOL service with 2yr duration...", 2);
+        startDate.add(Calendar.YEAR, -2);
+    } else if (pptAcctAge >= 12) {
+        logDebug(key, "Participant's account age is >=12 months, making a call to SOL service with 1yr duration...", 1);
+        startDate.add(Calendar.YEAR, -1);
+    } else if (pptAcctAge >= 6) {
+        logDebug(key, "Participant's account age is >=6 months, making a call to SOL service with 6 months duration...", 6);
+        startDate.add(Calendar.MONTH, -6);
+    } else if (pptAcctAge >= 3) {
+        logDebug(key, "Participant's account age is >=3 months, making a call to SOL service with 3 months duration...", 3);
+        startDate.add(Calendar.MONTH, -3);
+    } else {
+        startDate = null;
+        endDate = null;
+    }
+
+    if (startDate == null && endDate == null) {
+        balanceHistory = Collections.emptyMap();
+    } else {
+        try {
+            SolServices solService = SolServices.getInstance(key.getClient(), key.getPlan(), key.getParticipant());
+            balanceHistory = solService.getBalanceHistoryData(startDate, endDate);
+        } catch (BusinessException solException) {
+            logError("Exception occurred for Balance History Sol Service....", solException);
+            throw solException;
+        }
+    }
+}
+
+private void logDebug(ParticipantKey key, String message, int duration) {
+    if (LOGGER.isDebugEnabled()) {
+        LOGGER.debug("Participant's {} account age is >= {} months, {}", key.getParticipant(), duration, message);
+    }
+}
+
+private void logError(String message, BusinessException exception) {
+    if (LOGGER.isErrorEnabled()) {
+        LOGGER.error(message, exception);
+    }
+}
+
+
+
+
+
+
+
+
+
+
 Refactor this method to reduce its cognitive complexity
 
 
