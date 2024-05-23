3e7e8ee12b720454e274dce205029c588d22cb2f
Thanushbj7 <111106003+Thanushbj7@users.noreply.github.com>
2024-05-23 19:22:49 +0530

    Update CopyTableExtract.java

--- CopyTableExtract.java
+++ CopyTableExtract.java
@@ -1,3 +1,56 @@
+@AuraEnabled(cacheable=false)
+public static List<UltimatePopControllerHelper.SearchResult> initializeAndLoadPlanData(String clientId){
+    Case currentCase = new Case();
+    //system.debug('Acc Id '+clientId);
+	String clientSSN = '';
+	String test = '';
+
+    ////system.debug('id '+Id+' cientSSN '+clientSSN+' vru '+test);
+    //this.innerSharingClassInstance = new CTIWelcomePageController.CTIWelcomePageControllerWithSharing();
+    
+    ////system.debug('====================== ApexPages.currentPage().getParameters().get(id) ' + ApexPages.currentPage().getParameters().get('id'));
+    
+    CTI_Console_Pop__c  ctiConsolePop = [select Id, Account__r.SSN__c, CTI_Params__c, DC_Serialized_Result__c, Case__c, 
+        Case__r.Id, Case__r.CaseNumber, Offer_Pop__c, Offer_Pop__r.Id from CTI_Console_Pop__c 
+        where account__c = :clientId order by LastModifiedDate  desc limit 1];
+        String[] keyValuePairs = ctiConsolePop.CTI_Params__c.split(';');
+        for(String pair : keyValuePairs) {
+            String[] keyValue = pair.split(':');
+            if(keyValue.size() == 2) {
+                String key = keyValue[0].trim();
+                String value = keyValue[1].trim();
+        
+                if(key == 'clientId') {
+                    clientSSN = value;
+                } else if(key == 'VRUAPP') {
+                    test = value;
+                }
+            }
+        }
+        
+        //system.debug('clientSSN: ' + clientSSN);
+        //system.debug('test: ' + test);
+    Account client = UltimatePopControllerHelper.getSFDCClientInfo(ctiConsolePop.Account__r.SSN__c, true);
+    
+    List<Case> currentCaseList = [select Id, CaseNumber, Offers_Available__c from Case where Id = :ctiConsolePop.Case__r.Id];
+    if(currentCaseList != null && currentCaseList.size() > 0)
+        currentCase = currentCaseList.get(0);
+    	
+    String ctiParams = ctiConsolePop.CTI_Params__c;
+    CurCase=ctiConsolePop.Case__r.Id;
+  
+    return initializeAvailablePlans(currentCase,clientSSN,test,client);
+    
+
+
+
+
+
+
+}
+
+
+
 @isTest
 private class TestUltimatePopController {
 
