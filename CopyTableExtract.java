5084ce237014c3df7376daa1069a45c34e99c4d4
Thanushbj7 <111106003+Thanushbj7@users.noreply.github.com>
2024-05-24 17:01:26 +0530

    Update CopyTableExtract.java

--- CopyTableExtract.java
+++ CopyTableExtract.java
@@ -1,3 +1,65 @@
+ public static Map<Id,String> getNomalizedScore(Set<Id> articleId){
+  	  
+      	Map<Id,Decimal> articleIdToNormScoreMap=new Map<Id,Decimal>();
+          Map<Id,String> articleIdToNormScoreMapNew=new Map<Id,String>();
+
+      if(Test.isRunningTest()){
+        articleIdToNormScoreMap.put([SELECT NormalizedScore, Id, ParentId FROM KnowledgeArticleViewStat WHERE ParentId in :articleId limit 1].Id,Decimal.valueOf(100));
+      }
+      else {
+      	List<KnowledgeArticleViewStat>  kavs = Database.query('SELECT NormalizedScore, Id, ParentId FROM KnowledgeArticleViewStat WHERE ParentId in :articleId  ORDER BY NormalizedScore desc');
+      	
+       
+        
+        
+        for( KnowledgeArticleViewStat cc:kavs ){
+            Integer normscore;
+            String StringValue;
+        normscore = cc.NormalizedScore.round().intValue();
+            
+      if(normscore < 20)
+        
+      StringValue = String.valueOf(0);
+        else if(normscore >= 20 && normscore < 40 )
+        StringValue =  String.valueOf(20);
+        
+        else if(normscore >= 40 && normscore < 60 )
+          
+        StringValue =  String.valueOf(40);
+        else if(normscore >= 60 && normscore < 80 )
+          
+        StringValue =  String.valueOf(60);
+        else if(normscore >= 80 && normscore <   100 )
+          
+        StringValue =  String.valueOf(80);
+        else
+          
+        StringValue = String.valueOf(100);
+
+        if (articleIdToNormScoreMap.containsKey(cc.ParentId) && articleIdToNormScoreMapNew.containsKey(cc.ParentId)) {
+            system.debug('Knowledge Test '+cc.parentId+' - '+articleIdToNormScoreMap.get(cc.ParentId));
+            Decimal currentScore = articleIdToNormScoreMap.get(cc.ParentId);
+            if (cc.normalizedScore > currentScore) {
+                articleIdToNormScoreMap.put(cc.ParentId, cc.normalizedScore);
+                articleIdToNormScoreMapNew.put(cc.parentId,StringValue);
+            }
+        } else {
+            articleIdToNormScoreMap.put(cc.ParentId, cc.normalizedScore);
+            articleIdToNormScoreMapNew.put(cc.parentId,StringValue);
+        }
+
+    }
+}
+return articleIdToNormScoreMapNew;
+  }
+
+
+
+
+
+
+
+
 @isTest
 public class KnowledgeArticleWrapperTest {
     
