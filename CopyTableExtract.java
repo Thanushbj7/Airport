104874360231f9e96b0c37c4b9f3b67b9b088f98
Thanushbj7 <111106003+Thanushbj7@users.noreply.github.com>
2024-05-29 13:45:43 +0530

    Update CopyTableExtract.java

--- CopyTableExtract.java
+++ CopyTableExtract.java
@@ -1,3 +1,44 @@
+List<knowledgeArticleWrapper> kaWrapperList = new List<knowledgeArticleWrapper>();
+    for (Knowledge__kav kav : knowledeArticles) {
+        knowledgeArticleWrapper kaWrap = new knowledgeArticleWrapper();
+        kaWrap.id = kav.Id;
+        kaWrap.title = kav.Title;
+        kaWrap.articleNumber = kav.ArticleNumber;
+        kaWrap.validationStatus = kav.ValidationStatus;
+        kaWrap.publishStatus = kav.PublishStatus;
+        kaWrap.knowledgeArticleId = kav.KnowledgeArticleId;
+        kaWrap.summary = kav.Summary;
+        kaWrap.recordTypeName = kav.RecordType.Name;
+        kaWrap.visibleInPublicKnowledgeBase = kav.IsVisibleInPkb;
+        kaWrap.viewCountDS = scoreMap.get(kav.KnowledgeArticleId);
+       
+        
+            kaWrap.displayGenericKnowArticles = true;
+        
+        
+            kaWrap.displayCopytoClipboardIcon = false;
+        
+         
+
+        if (kav.PublishStatus == 'Draft') {
+            kaWrap.articleAttachedToCase = false;
+        }
+        
+        if (kav.PublishStatus.equalsIgnoreCase('Online') && kaWrap.lastPublishedDate!=null) {
+            kaWrap.lastPublishedDate = kav.LastPublishedDate.format('MMM d, yyyy, HH:mm a');
+        } 
+        
+        if (kav.PublishStatus.equalsIgnoreCase('Draft')) {
+            kaWrap.lastModifiedDate = kav.LastModifiedDate.format('MMM d, yyyy, HH:mm a');
+        }
+        
+        kaWrapperList.add(kaWrap); 
+    }
+
+    return kaWrapperList;
+}
+
+
 @isTest
 private class UltimatePopControllerHelperTest {
 
