8ef78bbe76085c2dd9ebcec1e163f0a6e1ba6d85
Thanushbj7 <111106003+Thanushbj7@users.noreply.github.com>
2024-11-28 13:35:22 +0530

    Update CopyTableExtract.java

--- CopyTableExtract.java
+++ CopyTableExtract.java
@@ -1,1 +1,3 @@
+"Integrated the Genisys team into the retirement application by implementing a seamless workflow where, upon collection of basic customer details by Genisys representatives and confirming the data, the process transitions smoothly to the retirement application, ensuring a cohesive and efficient user experience."
+
 
